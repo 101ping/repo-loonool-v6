1f5f357f0c99560dd12236a36750e3881b662502
101ping <2557564161@qq.com>
2025-12-03 14:03:34 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
 document.getElementById("createSpaceBtn").addEventListener("click", () => {
-  // 未来你可以替换为：检查用户是否登录
-  const loggedIn = true; // 先简化处理：模拟已登录
+  const loggedIn = false; // TODO: 替换为真实登录状态
 
   if (!loggedIn) {
-    window.location.href = "/login.html";
+    window.location.href = "/login";
   } else {
-    // V1 你希望点击后直接进入 My Spaces
-    window.location.href = "/my-spaces.html";
+    window.location.href = "/review-space";
   }
 });
