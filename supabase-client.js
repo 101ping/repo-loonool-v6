635b6a108ff9dde9391edd9540cf5147cf115273
101ping <2557564161@qq.com>
2025-12-04 15:34:00 +0800

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,9 @@
 // supabase-client.js
 
 // TODO: 把下面两行改成你自己的项目配置（不要带引号错误）：
-const SUPABASE_URL = "https://你的-project-url.supabase.co";
-const SUPABASE_ANON_KEY = "你的-anon-public-key";
+const SUPABASE_URL = "https://iuvikcdtguhllpouduyu.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1dmlrY2R0Z3VobGxwb3VkdXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTEyNzcsImV4cCI6MjA4MDM4NzI3N30.Ye5Z8lQPefnXY0znP764TMX_vrwROQIzlBxoKflhz_8
+";
 
 // 创建一个全局可用的 supabase 客户端
 window.supabaseClient = window.supabase.createClient(
